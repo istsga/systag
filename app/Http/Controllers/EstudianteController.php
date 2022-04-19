01e1252de0deae0015e543f3e91d2ed4b3694780
daguapi11 <dguapicuji11@gmail.com>
2022-04-19 08:24:32 -0500

    SYSTAG v4.9

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -58,7 +58,8 @@ class EstudianteController extends Controller
     {
         $this->authorize('create', Estudiante::class);
         $estudiante = new Estudiante($request->validated());
-        $estudiante->foto = $request->file('foto')->store('images', 'public');
+        if($estudiante->foto == !null)
+            $estudiante->foto = $request->file('foto')->store('images', 'public');
         $estudiante->save();
         return redirect()->route('estudiantes.index')->with('status', 'Agregado con éxito');
     }
